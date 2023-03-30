e5d8a1551630b37604331f50d02332593d86e030
K0D3IN <128936399+K0D3IN@users.noreply.github.com>
2023-03-30 22:22:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,64 +1,78 @@
 from cryptography.fernet import Fernet
 import os
 
+def clear_screen():
+    os.system('clear')
+
+def print_title(title):
+    print(f"{'=' * 10} {title} {'=' * 10}\n")
+
+def encrypt_message():
+    clear_screen()
+    print_title("ENCRYPT A MESSAGE")
+
+    print("1) Encrypt with random key")
+    print("2) Encrypt with special key")
+    choice = input(">>> ")
+
+    if choice == '1':
+        key = Fernet.generate_key()
+    elif choice == '2':
+        key = input("Enter a key: ").encode()
+    else:
+        print("Invalid choice!")
+        return
+
+    fernet = Fernet(key)
+    message = input("Enter your message: ").encode()
+    encrypted_message = fernet.encrypt(message)
+
+    print(f"\nYOUR KEY IS = {key.decode()}")
+    print("THIS IS YOUR ENCRYPTED MESSAGE:")
+    print(encrypted_message.decode())
+
+def decrypt_message():
+    clear_screen()
+    print_title("DECRYPT A MESSAGE")
+
+    key = input("YOUR SPECIAL KEY>>>").encode()
+    message = input("YOUR MESSAGE>>>").encode()
+
+    fernet = Fernet(key)
+    decrypted_message = fernet.decrypt(message)
+
+    print(f"\nYOUR DECRYPTED MESSAGE IS: {decrypted_message.decode()}")
+
+def generate_random_key():
+    clear_screen()
+    print_title("GENERATE RANDOM KEY")
+
+    key = Fernet.generate_key()
+    print(key.decode())
 
 def main():
-    os.system("clear")
-    os.system("figlet CRYPT")
-    print('''CHOOSE OPTION:
-    1)ENCRYPT A MESSAGE
-    2)DECRYPT A MESSAGE
-    3)GENERATE RANDOM KEY
-    99)EXIT
-    ''')
-    opt = int(input(">>>"))
-    if opt == 1:
-        sifre()
-    elif opt == 2:
-        coz()
-    elif opt == 3:
-        random()
-    elif opt == 99:
-        exit()
+    while True:
+        clear_screen()
+        print_title("CRYPT")
 
+        print('''CHOOSE OPTION:
+        1) ENCRYPT A MESSAGE
+        2) DECRYPT A MESSAGE
+        3) GENERATE RANDOM KEY
+        99) EXIT\n''')
 
-def sifre():
-    os.system("clear")
-    os.system("figlet CRYPT")
-    print("1)Encrypt with random key\n2)Encrypt with special key")
-    c = int(input(">>>"))
-    if c == 1:
-        os.system("clear")
-        key = Fernet.generate_key()
-        fernet = Fernet(key)
-        message = input("YOUR MESSAGE>>>")
-        b = bytes(message, 'utf-8')
-        e_message = fernet.encrypt(b)
-        print("YOUR KEY IS =", key)
-        print("YOUR KEY IS WILL LOOK LIKE THIS b'xxx' DELETE THE 'b' AND QUOTATION MARKS")
-        print("THIS IS YOUR ENCRYPTED MESSAGE", e_message)
-    if c == 2:
-        os.system("clear")
-        key = input("YOUR KEY")
-        fernet = Fernet(key)
-        message = input("YOUR MESSAGE>>>")
-        e_message = fernet.encrypt(message)
-        print("THIS IS YOUR ENCRYPTED MESSAGE", e_message)
+        choice = input(">>> ").encode()
 
+        if choice == b'1':
+            encrypt_message()
+        elif choice == b'2':
+            decrypt_message()
+        elif choice == b'3':
+            generate_random_key()
+        elif choice == b'99':
+            exit()
+        else:
+            print("Invalid choice!")
 
-def coz():
-    os.system("clear")
-    key = input("YOUR SPECIAL KEY>>>")
-    message = input("YOUR MESSAGE>>>")
-    fernet = Fernet(key)
-    d_message = fernet.decrypt(message)
-    print(d_message)
-
-
-def random():
-    key = Fernet.generate_key()
-    print("YOUR KEY IS WILL LOOK LIKE THIS b'xxx' DELETE THE 'b' AND QUOTATION MARKS")
-    print(key)
-
-
-main()
+if __name__ == '__main__':
+    main()
